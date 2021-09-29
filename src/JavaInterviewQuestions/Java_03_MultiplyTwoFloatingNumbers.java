f85f59b886fbeec5c8b8d127bc64754099b68336
Elif Figanioglu <elif@figanioglu.com>
2021-09-29 20:00:28 +0200

    11. soru yeniden cozuldu

--- src/JavaInterviewQuestions/Java_03_MultiplyTwoFloatingNumbers.java
+++ src/JavaInterviewQuestions/Java_03_MultiplyTwoFloatingNumbers.java
@@ -7,6 +7,14 @@ public class Java_03_MultiplyTwoFloatingNumbers {
 	public static void main(String[] args) {
 		
 		//Question: Multiply two floating numbers
+		//elif
+
+		float x= 3.5475f;
+		float y=6.5577f;
+		float result = mult(x,y);
+		System.out.println(result);
+
+
 		/*
 		//huseyin tarafindan cozuldu
 		float a=1.2354f;
@@ -16,12 +24,14 @@ public class Java_03_MultiplyTwoFloatingNumbers {
 		*/
 
 		//Muhammet
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Geben Sie bitte zwei Zahlen(float) ein, um sie zu multiplizieren");
-		float zahl1 = scan.nextFloat();
-		float zahl2 = scan.nextFloat();
-		System.out.println("Die Multiplikation ist : " + (zahl1*zahl2));
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("Geben Sie bitte zwei Zahlen(float) ein, um sie zu multiplizieren");
+//		float zahl1 = scan.nextFloat();
+//		float zahl2 = scan.nextFloat();
+//		System.out.println("Die Multiplikation ist : " + (zahl1*zahl2));
 
 	}
-
+	public static float mult(float x, float y){
+		return x*y;
+	}
 }
