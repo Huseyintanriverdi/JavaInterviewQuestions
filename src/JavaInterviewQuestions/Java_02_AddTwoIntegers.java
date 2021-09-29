76aee111e61eb5141fa05bc9aa38516ccc8d966c
Elif Figanioglu <elif@figanioglu.com>
2021-09-29 20:09:44 +0200

    11. soru yeniden cozuldu

--- src/JavaInterviewQuestions/Java_02_AddTwoIntegers.java
+++ src/JavaInterviewQuestions/Java_02_AddTwoIntegers.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Java_02_AddTwoIntegers {
 
 	public static void main(String[] args) {
-		
+
 		//Question: Add two integer
+		//elif
+
+		System.out.println(sum(5, 7));
+
+
 		/*
 		//huseyin tarafindan cozuldu
 		int a = 10;
@@ -16,15 +21,20 @@ public class Java_02_AddTwoIntegers {
 		*/
 
 		//Muhammet
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Geben Sie bitte zwei Zahlen ein, um sie zu summieren");
-		int zahl1=scan.nextInt();
-		int zahl2=scan.nextInt();
-		System.out.println("Die Summe ist : " + (zahl1+zahl2));
+//		Scanner scan=new Scanner(System.in);
+//		System.out.println("Geben Sie bitte zwei Zahlen ein, um sie zu summieren");
+//		int zahl1=scan.nextInt();
+//		int zahl2=scan.nextInt();
+//		System.out.println("Die Summe ist : " + (zahl1+zahl2));
 
 
 
 
+	}
+
+	public static int sum(int x, int y){
+
+		return x+y;
 	}
 
 }
