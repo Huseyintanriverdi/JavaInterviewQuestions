25eef08ae5293a829a4564ba3dbf00e684a2f869
mhmmtyksl <mhmmt.yksl1@gmail.com>
2021-09-29 14:46:16 +0200

    29.09-2

--- src/JavaInterviewQuestions/Java_08_AlphabetVowelOrConsonant.java
+++ src/JavaInterviewQuestions/Java_08_AlphabetVowelOrConsonant.java
@@ -1,12 +1,15 @@
 package JavaInterviewQuestions;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Java_08_AlphabetVowelOrConsonant {
 
 	public static void main(String[] args) {
 		
       //Question: Check whether an alphabet is Vowel or Consonant
+		/*
 		// a e i o u
-		
 		char ch = 'c';
 		
 		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
@@ -15,7 +18,7 @@ public class Java_08_AlphabetVowelOrConsonant {
 		else {
 			System.out.println(ch + " is consonant");
 		}
-		
+
 		//Second way
 		switch (ch) {
 		case 'a':
@@ -29,6 +32,19 @@ public class Java_08_AlphabetVowelOrConsonant {
 		default:
 			System.out.println(ch + " is consonant");
 			break;
+		}
+
+		 */
+
+		//Muhammet
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Geben Sie einen Buchstaben ein, ob er Vokal oder Konsonant ist");
+		char a=scan.next().toLowerCase().charAt(0);
+
+		if (a =='a' || a =='e' || a =='i' || a =='o' || a =='ö' || a =='u' || a =='ü' || a =='ä' ||a =='y') {
+			System.out.println("Der eingegebene Buchstabe " + a + " ist ein Vokal");
+		} else {
+			System.out.println("Der eingegebene Buchstabe " + a + " ist ein Konsonant");
 		}
 
 	}
