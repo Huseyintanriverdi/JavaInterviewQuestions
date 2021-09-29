5f7b32a0fd1f5cbd0c47154b718c41d7f59a18e3
Elif Figanioglu <elif@figanioglu.com>
2021-09-29 20:48:04 +0200

    6. soru yeniden cozuldu

--- src/JavaInterviewQuestions/Java_06_SwapTwoNumbers.java
+++ src/JavaInterviewQuestions/Java_06_SwapTwoNumbers.java
@@ -3,41 +3,59 @@ package JavaInterviewQuestions;
 public class Java_06_SwapTwoNumbers {
 
 	public static void main(String[] args) {
-		
-		
-		//Swap numbers with temporary variable
-		
-		int a = 10;
-		int b = 20;
-		
-		System.out.println("before swapping: ");
-		System.out.println("Value of a: "+ a);
-		System.out.println("Value of b: "+ b);
-		
-		System.out.println("After swapping: ");
-		
-		int temp;
-		temp  = a;
-		a = b;
-		b = temp;
-		
-		System.out.println("Value of a: "+ a);
-		System.out.println("Value of b: "+ b);
-		
-		//swap two numbers without using a temporary
-		
-		System.out.println("before swapping: ");
-		System.out.println("Value of a: "+ a);
-		System.out.println("Value of b: "+ b);
-		
-		System.out.println("After swapping: ");
-		
-		a = a-b;
-		b = a+b;
-		a= b-a;
-		
-		System.out.println("Value of a: "+ a);
-		System.out.println("Value of b: "+ b);
+		//		//Swap numbers with temporary variable
+
+		//	elif
+		int a=5, b=10, temp=0;
+		temp=a;
+		a=b;
+		b=temp;
+
+		// gecici degisken olmadan
+		a=a+b;
+		b=a-b;
+		a=a-b;
+
+
+
+		System.out.println(a);
+		System.out.println(b);
+
+
+
+
+//
+//		int a = 10;
+//		int b = 20;
+//
+//		System.out.println("before swapping: ");
+//		System.out.println("Value of a: "+ a);
+//		System.out.println("Value of b: "+ b);
+//
+//		System.out.println("After swapping: ");
+//
+//		int temp;
+//		temp  = a;
+//		a = b;
+//		b = temp;
+//
+//		System.out.println("Value of a: "+ a);
+//		System.out.println("Value of b: "+ b);
+//
+//		//swap two numbers without using a temporary
+//
+//		System.out.println("before swapping: ");
+//		System.out.println("Value of a: "+ a);
+//		System.out.println("Value of b: "+ b);
+//
+//		System.out.println("After swapping: ");
+//
+//		a = a-b;
+//		b = a+b;
+//		a= b-a;
+//
+//		System.out.println("Value of a: "+ a);
+//		System.out.println("Value of b: "+ b);
 		
 		
 
