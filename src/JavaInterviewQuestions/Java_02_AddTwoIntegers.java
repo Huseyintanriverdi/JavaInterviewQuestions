c8302fb7527ca7f9094b44473fecc30ce7516d6b
Your Name <ilhami_3455@hotmail.com>
2021-10-03 14:43:08 +0200

    Soru 2 cozuldu.

--- src/JavaInterviewQuestions/Java_02_AddTwoIntegers.java
+++ src/JavaInterviewQuestions/Java_02_AddTwoIntegers.java
@@ -2,17 +2,24 @@ package JavaInterviewQuestions;
 
 public class Java_02_AddTwoIntegers {
 
-	public static void main(String[] args) {
-		
+	/*public static void main(String[] args) {
+
 		//Question: Add two integer
-		//huseyin tarafindan cozuldu
+	 	//huseyin tarafindan cozuldu
 		int a = 10;
 		int b = 20;
 		int sum = a+b;
 		System.out.println("a ile b nin toplami: "+ sum);
 
+	} */
 
+	// Canan cozum;
+	public static void main(String[] args) {
+		System.out.println("Sayilarin toplami: " + sumTwoNumber(30, 20));
 
 	}
 
+	public static int sumTwoNumber(int a, int b){
+		return a+b;
+	}
 }
